3c526e0b0b8d646b71f99c25d2955b2d8ef7dfff
Kelechukwu <kelebrizzle@gmail.com>
2023-08-23 02:53:46 -0700

    betty pass code

---
+++ betty.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * Main - Code to pass betty
+ *
+ * Return - always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("Pass Betty\n");
+	}
+
+	return 0;
+}
