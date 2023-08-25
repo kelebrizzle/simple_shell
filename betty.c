45491d181c2bbb43a36e9980d9f917ace760fec4
Kelechukwu <kelebrizzle@gmail.com>
2023-08-24 23:59:24 -0700

    "commit"

---
+++ betty.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Betty pass
+ *
+ * Return: always (0)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
